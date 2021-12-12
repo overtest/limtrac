c9ea3878ea80a98580cb224e28652b2440f0d5ba
Yurii Kadirov <admin@sirkadirov.com>
2021-12-12 15:35:15 +0200

    Code structure improvements, fixed #2 and some new features
    1. Sightly improved source code structure;
    2. Unset `limtrac` environment vars before user-defined program startup;
    3. Allow enabling/disabling features and configuring resource limits by setting environment variables;
    4. Check pointer for `NULL` after calling `getpwnam`;
    5. Update README file.

--- README.md
+++ README.md
@@ -1,22 +1,32 @@
-# LimTrac
+# ⚡ Limtrac
 
 **LimTrac** is a simple program written in Rust, designed for GNU/Linux-based operation systems, which enforces process security policies (using such GNU/Linux capabilities as `seccomp`, `prlimit`, `cgroups`, etc) to the current running process and executes a requested program with specified arguments as the specified user.
 
-### Features
+### ✨ Features
 
 `Limtrac` is a part of [Overtest](https://github.com/overtest) free software project, and is being used by Overtest Verification Agent (also known as `overtest-agent`) for untrusted programs execution, so it contains only features, needed by `overtest-agent` in GNU/Linux environment. For now, the quickest way to determine available features list is to browse the source code of the program.
 
-**WARNING:** As it said before, `limtrac` is only a third-party dependence for Overtest, so its features depend on the main project needs. Feel free to use `limtrac` source code base to create your own apps - it is the safest way to use its features.
+**WARNING:** As it said before, `limtrac` is only a third-party dependency for Overtest, so its features depend on the main project needs. Feel free to use `limtrac` source code base to create your own apps - it is the safest way to use its features.
 
-### Building
+### 🏗 Building
 
-You can only build `limtrac` in GNU/Linux environment (WSL also supported). To do it, use standard Cargo build command (for debug purposes): `cargo build`. If you need a release build, use this command: `cargo build --release`.
+You can only build `limtrac` in GNU/Linux environment (Windows Subsystem for Linux is also supported 😉). To build an executable, use standard Cargo build command:
 
-### Executing
+- **Debug:** `cargo build`
 
-Overtest executes `limtrac` using [CliWrap](https://github.com/Tyrrrz/CliWrap) (a library for .NET applications) with I/O streams piping and setting these environment variables (setting them are required):
+- **Release:** `cargo build --release`
 
-```powershell
+**Tip:** Use [JetBrains CLion](https://jetbrains.com/clion/) with official Rust plugin & WSL 2 to build `limtrac` 😃!
+
+### ⚙ Executing
+
+[Overtest](https://github.com/overtest/overtest) executes `limtrac` using [CliWrap](https://github.com/Tyrrrz/CliWrap) (a library for cross-platform .NET applications) with I/O streams piping and setting these environment variables:
+
+```bash
+# ###################################
+# # Required environment variables: #
+# ###################################
+
 # A string containing a full path to the executable.
 LIMTRAC_FULLPATH = "/usr/bin/example_program"
 
@@ -28,11 +38,33 @@ LIMTRAC_ARGUMENTS = "example_program first_argument second_argument"
 # A string which represents a "run as another user" feature.
 # If set to empty string, "run as" feature will be disabled.
 LIMTRAC_RUNAS = "testuser"
+
+# ######################################
+# # Unnecessary environment variables: #
+# ######################################
+
+# Enable or disable process resource limiting features,
+# see https://linux.die.net/man/2/setrlimit for docs
+DEFAULT_RLIM_ENABLED = "true"
+LIMTRAC_RLIM_CORE = "0"
+LIMTRAC_RLIM_NPROC = "1"
+LIMTRAC_RLIM_NOFILE = "10"
+
+# Disallow dangerous system calls usage (see sources)
+LIMTRAC_SCMP_ENABLED = "true"
+# Disallow move, remove and symlinks operations on file system
+LIMTRAC_SCMP_FS_GUARD = "true" # coming soon
 ```
 
-Also, when executing `limtrac`, you must set a working directory yourself. `Limtrac` not depends on any other files, so working directory can be safely set to specified in `LIMTRAC_FULLPATH` environment variable program's working directory.
+Also, when executing `limtrac`, you must set a working directory yourself. `Limtrac` not depends on any other files than a single executable and some system-wide shared libraries, so working directory can be safely set to specified in `LIMTRAC_FULLPATH` environment variable program's working directory.
 
-### License
+Before using `limtrac`, don't forget to add executable modificator to it:
+
+```bash
+chmod +x ./limtrac
+```
+
+### 📃 License
 
 ```
 LIMTRAC, a part of Overtest free software project.
@@ -45,7 +77,7 @@ License, or (at your option) any later version.
 
 This program is distributed in the hope that it will be useful,
 but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 GNU Affero General Public License for more details.
 
 You should have received a copy of the GNU Affero General Public License
--- src/cli/main.rs
+++ src/cli/main.rs
@@ -1,132 +1,149 @@
-use std::env;
+mod constants;
+mod structures;
+mod helpers;
+
 use std::ffi::CString;
 use std::io::{Error, ErrorKind};
 use nix::errno::errno;
+use syscallz::Syscall;
+use crate::structures::ProcExecRequest;
 
 fn main() {
-    let exec_request = ProcExecRequestInt
-    {
-        full_path: env::var("LIMTRAC_FULLPATH").expect("LIMTRAC_FULLPATH not set!"),
-        arguments: env::var("LIMTRAC_ARGUMENTS").expect("LIMTRAC_ARGUMENTS not set!"),
-        runas_user: env::var("LIMTRAC_RUNAS").expect("LIMTRAC_RUNAS not set!")
-    };
-
-    unsafe { execute_process(exec_request); }
+    // Load execution request details from environment variables
+    let exec_request = ProcExecRequest::new();
+    // Clean environment variables used by limtrac
+    helpers::clean_environment_variables("LIMTRAC_");
+    // Execute process in secure environment
+    execute_process(exec_request);
 }
 
-struct ProcExecRequestInt
+fn execute_process(exec_request: ProcExecRequest)
 {
-    pub full_path: String,
-    pub arguments: String,
-    //pub work_dir: String,
-    pub runas_user: String
-}
-
-unsafe fn execute_process(exec_request: ProcExecRequestInt)
-{
-    let argv = get_strings(exec_request.arguments.as_str());
-    child_pre_execute(&exec_request).unwrap();
-    
-    let c_fullpath = CString::new(exec_request.full_path.as_str()).unwrap();
-    if nix::libc::execv(c_fullpath.as_ptr(), argv) == -1
-    {
-        panic!("Program execution failed with ERRNO {}! Refer to <errno.h> to troubleshoot this issue.", errno());
-        // https://android.googlesource.com/kernel/lk/+/dima/for-travis/include/errno.h
-    }
-}
-
-unsafe fn get_strings(in_str : &str) -> *const *const nix::libc::c_char
-{
-    let mut v = vec![];
-    
-    if !(in_str.trim().is_empty())
-    {
-        let str_arr = in_str.split_whitespace();
-        for substr in str_arr
+    // Set up resource limiting features
+    prepare_environment(&exec_request).unwrap();
+    unsafe {
+        // Prepare program execution parameters
+        let program_fullpath = CString::new(exec_request.full_path.as_str()).unwrap();
+        // Argv always must be specified and must contain at least one element (name of the executable)
+        let program_argv = helpers::str_explode_to_native_list(exec_request.arguments.as_str());
+        // Execute requested program
+        if nix::libc::execv(program_fullpath.as_ptr(), program_argv) == -1
         {
-            v.push(CString::new(substr).unwrap());
+            panic!("Program execution failed with ERRNO {}! Refer to <errno.h> to troubleshoot this issue.", errno());
+            // https://android.googlesource.com/kernel/lk/+/dima/for-travis/include/errno.h
         }
     }
-
-    let mut out = v
-        .into_iter()
-        .map(|s| s.as_ptr())
-        .collect::<Vec<_>>();
-
-    out.push(std::ptr::null());
-    out.shrink_to_fit();
-
-    let ptr = out.as_ptr();
-    std::mem::forget(out);
-
-    return ptr;
 }
 
-unsafe fn child_pre_execute(exec_request: &ProcExecRequestInt) -> Result<(), std::io::Error>
+fn prepare_environment(exec_request: &ProcExecRequest) -> Result<(), std::io::Error>
 {
-    init_cgroups();
-    //nix::libc::unshare(nix::libc::CLONE_NEWNET);
-    init_prlimit().unwrap();
-    init_setuid(exec_request).unwrap();
-    init_scmp();
-    
-    fn init_cgroups()
-    {
-        //let nanoid_gen = nanoid!(10);
-        //let cgroup_id = nanoid_gen.as_str();
-        //let hierarchy = cgroups_rs::hierarchies::auto();
-        //let cg_builder = CgroupBuilder::new(&cgroup_id)
-        //    .devices()
-        //        // https://www.kernel.org/doc/html/v4.11/admin-guide/devices.html
-        //        .done();
+    //init_cgroups();
+    unsafe { // These functions use unsafe system calls
+        init_prlimit(exec_request).unwrap();
+        init_setuid(exec_request).unwrap();
     }
+    init_scmp(exec_request).unwrap();
+
+    //fn init_cgroups()
+    //{
+    //    let nanoid_gen = constants::DEFAULT_CGROUP_NAME;
+    //    let cgroup_id = nanoid_gen.as_str();
+    //    let hierarchy = cgroups_rs::hierarchies::auto();
+    //    let cg_builder = CgroupBuilder::new(&cgroup_id)
+    //        .devices()
+    //            // https://www.kernel.org/doc/html/v4.11/admin-guide/devices.html
+    //            .done();
+    //}
     
-    unsafe fn init_prlimit() -> Result<(), std::io::Error>
+    unsafe fn init_prlimit(exec_request: &ProcExecRequest) -> Result<(), std::io::Error>
     {
-        let rlimit_core = nix::libc::rlimit64{ rlim_cur: 0, rlim_max: 0 };
-        let rlimit_nproc = nix::libc::rlimit64{ rlim_cur: 0, rlim_max: 0 /* current process */ + 0 /* executed process */ };
-        
-        // RLIMIT_CORE
+        if !((*exec_request).rlimit_enabled) { return Ok(()); }
+
+        /*
+         * RLIMIT_CORE
+         */
+        let rlimit_core = nix::libc::rlimit64
+        {
+            rlim_cur: (*exec_request).rlimit_core as nix::libc::rlim64_t,
+            rlim_max: (*exec_request).rlimit_core as nix::libc::rlim64_t
+        };
         if nix::libc::setrlimit64(nix::libc::RLIMIT_CORE, &rlimit_core) != 0
-        { return Err(Error::new(ErrorKind::Other, "System call SETRLIMIT64 failed!")); }
-        
-        // RLIMIT_NPROC
+        { return Err(Error::new(ErrorKind::Other, "System call SETRLIMIT64 failed (RLIMIT_CORE)!")); }
+
+        /*
+         * RLIMIT_NPROC
+         */
+        let rlimit_nproc = nix::libc::rlimit64{
+            rlim_cur: (*exec_request).rlimit_nproc as nix::libc::rlim64_t,
+            rlim_max: (*exec_request).rlimit_nproc as nix::libc::rlim64_t
+        };
         if nix::libc::setrlimit64(nix::libc::RLIMIT_NPROC, &rlimit_nproc) != 0
-        { return Err(Error::new(ErrorKind::Other, "System call SETRLIMIT64 failed!")); }
+        { return Err(Error::new(ErrorKind::Other, "System call SETRLIMIT64 failed (RLIMIT_NPROC)!")); }
+
+        /*
+         * RLIMIT_NOFILE
+         */
+        let rlimit_nofile = nix::libc::rlimit64{
+            rlim_cur: (*exec_request).rlimit_nofile as nix::libc::rlim64_t,
+            rlim_max: (*exec_request).rlimit_nofile as nix::libc::rlim64_t
+        };
+        if nix::libc::setrlimit64(nix::libc::RLIMIT_NOFILE, &rlimit_nofile) != 0
+        { return Err(Error::new(ErrorKind::Other, "System call SETRLIMIT64 failed (RLIMIT_NOFILE)!")); }
         
         Ok(())
     }
     
-    fn init_scmp()
+    fn init_scmp(exec_request: &ProcExecRequest) -> Result<(), std::io::Error>
     {
+        if !((*exec_request).scmp_enabled) { return Ok(()); }
+
+        unsafe {
+            // Unshare internet connection with the current program
+            nix::libc::unshare(nix::libc::CLONE_NEWNET);
+        }
+
         /* We use SYSCALLZ crate as the lbseccomp binding: https://docs.rs/syscallz/ */
         
         // We need to add all unwanted syscalls to this list
         let disallowed_syscalls = [
-            syscallz::Syscall::fork, syscallz::Syscall::vfork, syscallz::Syscall::clone, syscallz::Syscall::clone3, // process fork & clone
-            syscallz::Syscall::reboot, syscallz::Syscall::setuid, syscallz::Syscall::setgid, syscallz::Syscall::setrlimit,
-            //syscallz::Syscall::getrlimit, syscallz::Syscall::prlimit64, // resource limiting
-            syscallz::Syscall::chmod, syscallz::Syscall::fchmod, syscallz::Syscall::fchmodat, // chmod
-            syscallz::Syscall::chown, syscallz::Syscall::fchown, syscallz::Syscall::lchown, syscallz::Syscall::fchownat // chown
+            Syscall::fork, Syscall::vfork, Syscall::clone, Syscall::clone3, // process fork & clone
+            Syscall::reboot, Syscall::setuid, Syscall::setgid, Syscall::setrlimit,
+            //Syscall::getrlimit, Syscall::prlimit64, // resource limiting
+            Syscall::chmod, Syscall::fchmod, Syscall::fchmodat, // chmod
+            Syscall::chown, Syscall::fchown, Syscall::lchown, Syscall::fchownat, // chown
+            //Syscall::link, Syscall::unlink
         ];
+
         // Initialize a new SECCOMP context
         let mut ctx = syscallz::Context::init_with_action(syscallz::Action::Allow).unwrap();
+
         // Add restriction rules to the created context
         for sys_call in disallowed_syscalls {
             ctx.set_action_for_syscall(syscallz::Action::KillProcess, sys_call).unwrap();
         }
+
         // Enforce the SECCOMP policy we built
         ctx.load().unwrap();
+
+        Ok(())
     }
     
-    unsafe fn init_setuid(exec_request: &ProcExecRequestInt) -> Result<(), std::io::Error>
+    unsafe fn init_setuid(exec_request: &ProcExecRequest) -> Result<(), std::io::Error>
     {
         if (*exec_request).runas_user.is_empty() { return Ok(()) }
 
-        let usrname = CString::new((*exec_request).runas_user.as_str()).unwrap();
-        
+        let raw_username = CString::new((*exec_request).runas_user.as_str()).unwrap();
         // Get PASSWD information about the user behind the username
-        let pwnam = nix::libc::getpwnam(usrname.as_ptr());
+        let pwnam = nix::libc::getpwnam(raw_username.as_ptr());
+
+        // Check whether specified user not found
+        if pwnam.is_null()
+        {
+            return Err(Error::new(
+                ErrorKind::Other,
+                format!("System call GETPWNAM failed! User '{}' was not found!", (*exec_request).runas_user)
+            ));
+        }
         
         // Try to execute SETUID system call on the current process
         if nix::libc::setuid((*pwnam).pw_uid) != 0

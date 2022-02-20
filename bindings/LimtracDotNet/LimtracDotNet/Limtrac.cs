8b9b939d72fd5b3172b37f7ebd340d51e2315f67
Yurii Kadirov <admin@sirkadirov.com>
2022-02-20 14:35:25 +0200

    Finished work on .NET binding, project structure improvements

---
+++ bindings/LimtracDotNet/LimtracDotNet/Limtrac.cs
@@ -0,0 +1,60 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using System.Runtime.InteropServices;
+using Sirkadirov.Libraries.Limtrac.RequestStructs;
+using Sirkadirov.Libraries.Limtrac.ResultStructs;
+
+namespace Sirkadirov.Libraries.Limtrac;
+
+[SuppressMessage("ReSharper", "UnusedType.Global")]
+[SuppressMessage("ReSharper", "UnusedMember.Global")]
+public class Limtrac
+{
+    private ExecProgInfo   _execProgInfo;
+    private ExecProgIO     _execProgIo;
+    private ExecProgLimits _execProgLimits;
+    private ExecProgGuard  _execProgGuard;
+    
+    private Limtrac() {  }
+    public static Limtrac Prepare() { return new Limtrac(); }
+    
+    public Limtrac WithProgramInfo(ExecProgInfo execProgInfo)
+    {
+        _execProgInfo = execProgInfo;
+        return this;
+    }
+    
+    public Limtrac WithIoConfig(ExecProgIO execProgIo)
+    {
+        _execProgIo = execProgIo;
+        return this;
+    }
+
+    public Limtrac WithLimits(ExecProgLimits execProgLimits)
+    {
+        _execProgLimits = execProgLimits;
+        return this;
+    }
+
+    public Limtrac WithGuard(ExecProgGuard execProgGuard)
+    {
+        _execProgGuard = execProgGuard;
+        return this;
+    }
+
+    private void ThrowIfNotReadyToExecute()
+    {
+        ArgumentNullException.ThrowIfNull(_execProgInfo,   nameof(ExecProgInfo));
+        ArgumentNullException.ThrowIfNull(_execProgIo,     nameof(ExecProgIO));
+        ArgumentNullException.ThrowIfNull(_execProgLimits, nameof(ExecProgLimits));
+        ArgumentNullException.ThrowIfNull(_execProgGuard,  nameof(ExecProgGuard));
+    }
+
+    public ProcExecResult Execute()
+    {
+        if (!RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
+            throw new PlatformNotSupportedException("Limtrac is available only on Linux!");
+        ThrowIfNotReadyToExecute();
+        
+        return LimtracInterop.Execute(_execProgInfo!, _execProgIo!, _execProgLimits!, _execProgGuard!);
+    }
+}
---
+++ bindings/LimtracDotNet/LimtracDotNet/ResultStructs/ProcResUsage.cs
@@ -0,0 +1,14 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using System.Runtime.InteropServices;
+
+namespace Sirkadirov.Libraries.Limtrac.ResultStructs;
+
+[StructLayout(LayoutKind.Sequential)]
+[SuppressMessage("ReSharper", "MemberCanBePrivate.Global")]
+[SuppressMessage("ReSharper", "FieldCanBeMadeReadOnly.Global")]
+public struct ProcResUsage
+{
+    [MarshalAs(UnmanagedType.U8)] public ulong real_time;
+    [MarshalAs(UnmanagedType.U8)] public ulong proc_time;
+    [MarshalAs(UnmanagedType.U8)] public ulong proc_wset;
+}
